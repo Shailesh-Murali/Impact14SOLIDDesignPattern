1368f99d2e41ad3d5efdcbd95bb9575baa704c9e
Shailesh-Murali <shaileshmurali66@gmail.com>
2022-08-03 18:27:08 +0530

    day 2 case study

---
+++ casestudy_day2/src/task/aug2/CustomException.java
@@ -0,0 +1,28 @@
+package task.aug2;
+
+class InvalidSeatNumberException extends RuntimeException {
+	
+	public InvalidSeatNumberException(String message) 
+	{
+		super(message);
+	}
+
+}
+
+class SeatsNotAvailableException  extends RuntimeException {
+	
+	public SeatsNotAvailableException (String message) 
+	{
+		super(message);
+	}
+
+}
+
+class UnknownShowException extends RuntimeException {
+	
+	public UnknownShowException(String message) 
+	{
+		super(message);
+	}
+
+}
---
+++ casestudy_day2/src/task/aug2/DisplayShow.java
@@ -0,0 +1,10 @@
+package task.aug2;
+
+import java.util.List;
+
+@FunctionalInterface
+public interface DisplayShow {
+
+	public void displayAllShows(List<Show> shows); 
+	
+}
